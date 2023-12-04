e683d4e6c684861122068267bc4bbb84f882b791
osasou <osasou0125@gmail.com>
2023-12-05 00:22:55 +0900

    ✨ add links

--- src/app/customer/[customerId]/page.tsx
+++ src/app/customer/[customerId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useCustomer } from "../../../../hooks/customer/useCustomer";
 import { CustomerInfo } from "./CustomerInfo";
 import { RankingInfo } from "./RankingInfo";
@@ -8,6 +9,9 @@ export default function Customer() {
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ fontSize: "20px" }}>
+        <Link href="/">Home</Link>
+      </div>
       <CustomerInfo customerInfo={customerInfo} />
       <br />
       <RankingInfo customerInfo={customerInfo} rankingInfo={rankingInfo} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main style={{ display: "flex", flexDirection: "column" }}>
+      Home
+      <Link href="/customer/1">customer1</Link>
+      <Link href="/customer/2">customer2</Link>
+      <Link href="/customer/3">customer3</Link>
+      <Link href="/customer/4">customer4</Link>
+    </main>
+  );
 }
